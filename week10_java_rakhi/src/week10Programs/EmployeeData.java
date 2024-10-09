b5bf4b3145b68c59968c78809e95440a2b80e7de
Mounika7995 <141722028+Mounika7995@users.noreply.github.com>
2024-10-09 10:37:23 +0530

    Update EmployeeData.java

--- week10_java_rakhi/src/week10Programs/EmployeeData.java
+++ week10_java_rakhi/src/week10Programs/EmployeeData.java
@@ -15,7 +15,7 @@ public class EmployeeData {
 		employeeList.add(new Employee(4, "Mahesh", 28, "Male", "Development", 2014, 32500.0));
 		employeeList.add(new Employee(5, "Preethi", 27, "Female", "HR", 2013, 22700.0));
 		employeeList.add(new Employee(6, "Gopi", 43, "Male", "Accounts", 2016, 10500.0));
-    employeeList.add(new Employee(7, "KamalNarayan", 35, "Male", "Finance", 2010, 27000.0));
+    		employeeList.add(new Employee(7, "KamalNarayan", 35, "Male", "Finance", 2010, 27000.0));
 		employeeList.add(new Employee(8, "Suresh", 31, "Male", "Development", 2015, 34500.0));
 		employeeList.add(new Employee(9, "AKhila", 24, "Female", "Marketing", 2016, 11500.0));
 		employeeList.add(new Employee(10, "Arjun Kumar", 38, "Male", "Sales", 2015, 11000.5));
